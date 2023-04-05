5fb8ada533cb4dcf39aaef0dfe7cd16a00924069
Muhammad Umair Yaqub <umairyaqubofficial@gmail.com>
2023-04-05 17:12:05 +0500

    Update Web_Traffic.py

--- Web_Traffic.py
+++ Web_Traffic.py
@@ -8,34 +8,40 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-# from webdriver_manager.chrome import ChromeDriverManager as CM
 from webdriver_manager.microsoft import EdgeChromiumDriverManager as CM
 
-print("A R K W A R D   B O O S T\nCopyright (C) Ark Corporation. All rights reserved.\n\nD E F Y   R E A L I T Y\n\n")
+# Print welcome message
+print("A R K W A R D   B O O S T\nCopyright (C) Ark Corporation. All rights reserved.")
+print("\nD E F Y   R E A L I T Y\n\n")
 
-Views = input("How many user you want :: ")
+# Prompt user for input
+Views = input("How many users do you want? :: ")
 Views = int(Views)
 URL = input("Enter the URL :: ")
 
-options = webdriver.ChromeOptions()
+# Set up browser options
 options = webdriver.EdgeOptions()
-# options.add_argument("--headless")
 
+# Set up mobile emulation
 mobile_emulation = {
     "userAgent": 'Mozilla/5.0 (Linux; Android 4.0.3; HTC One X Build/IML74K) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/83.0.1025.133 Mobile Safari/535.19'
 }
 options.add_experimental_option("mobileEmulation", mobile_emulation)
-# bot = webdriver.Chrome(executable_path=CM().install(), options=options)
+
+# Set up the driver for the specified browser
 bot = webdriver.Edge(executable_path=CM().install(), options=options)
 
+# Navigate to the specified URL
 # bot.get(URL)
 
+# Set the window size and wait for the page to load
 bot.set_window_size(500, 950)
 time.sleep(5)
-bot.get('www.youtube.com')
+bot.get('https://www.youtube.com')
 time.sleep(5)
 
+# Loop through the specified number of views and refresh the page
 for i in range(Views):
-    print("S U C E S S F U L")
+    print("S U C C E S S F U L")
     time.sleep(10)
     bot.refresh()
